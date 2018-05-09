5b9d272a523216e17f77a8a32f5643c9e72666dc
tonygustafsson <tony.gustafsson@starrepublic.com>
2018-05-09 14:11:44 +0200

    Try to remove git connection

---
+++ php-checkout-sdk2/tests/Authenticate/AuthenticateTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+use Sdk\AccessToken;
+use Sdk\Api;
+
+class AuthenticateTest extends TestCase
+{
+    public function testGetAuthToken()
+    {
+        $accessToken = new AccessToken('', '', true);
+        $this->assertEquals($accessToken->get_token(), null);
+    }
+}
