36c9d9c109f15c1e2a191273cb0c86db73aae7e5
Conrad Ohlsson <lusffs@gmail.com>
2018-05-25 10:54:49 +0200

    Change urls to access production environment instead of test

--- php-checkout-sdk/config.php
+++ php-checkout-sdk/config.php
@@ -2,8 +2,8 @@
 function wasa_config($key = '')
 {
     $wasa_configuration = [
-        'base_url' => 'http://wkcloud-front-lb-1268786071.eu-central-1.elb.amazonaws.com:82',
-        'access_token_url' => 'http://wkcloud-front-lb-1268786071.eu-central-1.elb.amazonaws.com:83/connect/token',
+        'base_url' => 'https://b2b.services.wasakredit.se',
+        'access_token_url' => 'https://b2b.services.wasakredit.se/auth/connect/token',
         'version' => 'php-2.2',
         'plugin' => 'woo-1.0'
     ];
