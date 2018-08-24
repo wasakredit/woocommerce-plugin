8e1e59fa184e15879278e377ef42986561f5af25
elias.ung <elias.ung@starrepublic.com>
2018-08-24 15:23:40 +0200

    Only display widget if settings is toggled, navigate to route in setting

--- templates/redirect-to-standard-checkout.php
+++ templates/redirect-to-standard-checkout.php
@@ -15,6 +15,18 @@ require_once plugin_dir_path( __FILE__ ) . '../php-checkout-sdk/Wasa.php';
 
 $settings = get_option( 'wasa_kredit_settings' );
 
+$enable_redirect_widget = $settings['add_redirect_to_standard_checkout_widget'];
+$redirect_route = $settings['standard_checkout_page_route']; 
+$current_route = end(array_filter(explode('/', get_permalink())));
+
+if ( ! isset( $enable_redirect_widget ) 
+    || 'yes' !== $enable_redirect_widget 
+    || ! isset( $redirect_route ) 
+    || $current_route === $redirect_route
+    )
+{
+    return;
+}
 // Connect WASA SDK client
 $client = new Sdk\Client(
 	$settings['partner_id'],
@@ -35,7 +47,7 @@ if ( isset( WC()->cart ) ) {
 <?php if ( isset( $payment_methods_response ) && 200 === $payment_methods_response->statusCode ) {  ?>
 <div style="border:1px solid #efefef;padding:15px;margin-bottom:40px;background-color:#fbfbfb">
     <span style="display:block"><strong>Finansiering</strong></span>
-    <a href="/?page_id=6" style="display:inline-block;float:right;color:#fff;background-color:#ad1015;padding:10px 20px">
+    <a href="<?php echo "/".$redirect_route ?>" style="display:inline-block;float:right;color:#fff;background-color:#ad1015;padding:10px 20px">
         <span><strong>Byt betalsätt</strong></span></br>
         <span>Finansiera med Wasa Kredit</span>
     </a>
