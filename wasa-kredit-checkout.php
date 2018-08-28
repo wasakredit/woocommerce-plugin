b78a5baef9df992264ffecbf875d9a8ed027498e
elias.ung <elias.ung@starrepublic.com>
2018-08-28 14:45:47 +0200

    Incremented plugin version

--- wasa-kredit-checkout.php
+++ wasa-kredit-checkout.php
@@ -37,7 +37,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'WASA_KREDIT_CHECKOUT_VERSION', '1.2.1' );
+define( 'WASA_KREDIT_CHECKOUT_VERSION', '1.2.2' );
 
 /**
  * The code that runs during plugin activation.
