76fbccbd1c22638eed0745b07e0c579dc408a1a0
Conrad Ohlsson <lusffs@gmail.com>
2018-09-04 13:12:44 +0200

    Change response code to match a successful result

--- includes/class-wasa-kredit-checkout-product-widget.php
+++ includes/class-wasa-kredit-checkout-product-widget.php
@@ -74,7 +74,7 @@ class Wasa_Kredit_Checkout_Product_Widget {
 		$price    = $product->get_price();
 		$response = $this->_client->get_monthly_cost_widget( $price );
 
-		if ( isset( $response ) && 201 === $response->statusCode ) { // @codingStandardsIgnoreLine - Our backend answers in with camelCasing, not snake_casing
+		if ( isset( $response ) && 200 === $response->statusCode ) { // @codingStandardsIgnoreLine - Our backend answers in with camelCasing, not snake_casing
 			return '<div class="wasa-kredit-product-widget-container">' . $response->data . '</div>';
 		}
 
