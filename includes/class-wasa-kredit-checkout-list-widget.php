1368780d19bf85d6ab3014549ac74fe5831664c7
nalezny <jaroslav@nalezny.cz>
2020-08-01 01:25:16 +0200

    Fixed missing leasing price in woocommerce shortcode [products]
    Improved fetching of leasing plans from API

--- includes/class-wasa-kredit-checkout-list-widget.php
+++ includes/class-wasa-kredit-checkout-list-widget.php
@@ -24,6 +24,12 @@ class Wasa_Kredit_Checkout_List_Widget {
 		);
 
 		add_action(
+			'woocommerce_shortcode_before_products_loop',
+			array( $this, 'save_product_prices_shortcodes' ),
+			10
+		);
+
+		add_action(
 			'woocommerce_after_shop_loop_item',
 			array( $this, 'display_leasing_price_per_product' ),
 			9
@@ -36,7 +42,7 @@ class Wasa_Kredit_Checkout_List_Widget {
 	}
 
 	public function display_leasing_price_per_product() {
-		// Adds financing info betweeen price and Add to cart button
+		// Adds financing info betweeen price and Add to cart button in category and [products] woocommerce shortcode
 		global $product;
 
 		$settings = get_option( 'wasa_kredit_settings' );
@@ -45,20 +51,26 @@ class Wasa_Kredit_Checkout_List_Widget {
 			return;
 		}
 
-		$monthly_cost = 0;
+		$current_currency = get_woocommerce_currency();
 
-		if ( isset( $GLOBALS['product_leasing_prices'] ) ) {
-			$monthly_cost = $GLOBALS['product_leasing_prices'][ $product->get_id() ];
+		$payload['items'][] = array(
+			'financed_price' => array(
+				'amount'   => round($product->price, 0),
+				'currency' => $current_currency,
+			),
+			'product_id'     => $product->id,
+		);
+
+		$response = $this->_client->calculate_monthly_cost($payload);
+
+		if ( isset( $response ) && 200 === $response->statusCode ) { // @codingStandardsIgnoreLine - Our backend answers in with camelCasing, not snake_casing
+			$amount = $response->data['monthly_costs'][0]['monthly_cost']['amount'];
+
+			echo '<p>' .
+				__( 'Financing', 'wasa-kredit-checkout' ) . ' <span style="white-space:nowrap;">' .
+				wc_price( $amount, array( 'decimals' => 0 ) ) . __( '/month', 'wasa-kredit-checkout' ) .
+				'</span></p>';
 		}
-
-		if ( $monthly_cost < 1 ) {
-			return;
-		}
-
-		echo '<p>' .
-			__( 'Financing', 'wasa-kredit-checkout' ) . ' <span style="white-space:nowrap;">' .
-			wc_price( $monthly_cost, array( 'decimals' => 0 ) ) . __( '/month', 'wasa-kredit-checkout' ) .
-			'</span></p>';
 	}
 
 	public function save_product_prices() {
@@ -75,6 +87,71 @@ class Wasa_Kredit_Checkout_List_Widget {
 		$current_currency = get_woocommerce_currency();
 
 		global $wp_query;		
+		$loop =  $wp_query;	
+
+		// Loop through all products
+		while ( $loop->have_posts() ) :
+			$loop->the_post();
+			global $product;
+
+			// Add this product to payload
+			$payload['items'][] = array(
+				'financed_price' => array(
+					'amount'   => round($product->get_price(), 2),
+					'currency' => $current_currency,
+				),
+				'product_id'     => $product->get_id(),
+			);
+		endwhile;
+
+		// Get resposne from API with all products defined in $payload
+		$response      = $this->_client->calculate_monthly_cost( $payload );
+		$monthly_costs = [];
+
+		if ( isset( $response ) && 200 === $response->statusCode ) {
+			foreach ( $response->data['monthly_costs'] as $current_product ) {
+				$monthly_costs[
+					$current_product['product_id']
+				] = $current_product['monthly_cost']['amount'];
+			}
+
+			// Save prices to global variable to access it from template
+			$GLOBALS['product_leasing_prices'] = $monthly_costs;
+		}
+	}
+
+	public function save_product_prices_shortcodes($args) {
+		// Collects all financing costs for all shown products
+		// Store as global variable to be accessed in display_leasing_price_per_product()
+		$settings = get_option( 'wasa_kredit_settings' );
+
+		if ( 'yes' !== $settings['widget_on_product_list'] ) {
+			return;
+		}
+
+		$payload['items'] = [];
+		// Payload will contain all products with price, currency and id
+		$current_currency = get_woocommerce_currency();
+
+		$args = array(
+		    'post_type' => 'product',
+		    'cat' => $args['category'],
+		    'page' => $args['page'],
+		    'orderby' => $args['orderby'],
+		    'order' => $args['order'],
+		    'posts_per_page' => $args['limit'],
+		    'tag' => $args['tag'],
+		);
+
+		if ( $args['page'] > 1 && $args['limit'] != -1) {
+			$args['paged'] = true;
+		}
+
+		if (!empty($args['ids'])) {
+			$args['post__in'] = array_map( 'trim', explode( ',', $args['ids'] ) );	
+		}
+
+		$wp_query = new WP_Query( $args );
 		$loop =  $wp_query;	
 
 		// Loop through all products
