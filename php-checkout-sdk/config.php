85ab934411b93c642ce404753f671f5942838c88
Gunnar Franklin <frankfurillo@hotmail.com>
2020-09-30 12:56:53 +0200

    #5668 mistake

--- php-checkout-sdk/config.php
+++ php-checkout-sdk/config.php
@@ -2,17 +2,13 @@
 function wasa_config($key = '')
 {
     $wasa_configuration = [
-        'base_url' => getEnvOrDefault('WASA_URI', 'http://st1-b2b.services.wasakredit.se'),
-        //'base_url' => getEnvOrDefault('WASA_URI', 'https://b2b.services.wasakredit.se'),
-        'access_token_url' => getEnvOrDefault('WASA_AUTH_URI', 'http://st1-b2b.services.wasakredit.se/auth/connect/token'),
-        //'access_token_url' => getEnvOrDefault('WASA_AUTH_URI', 'https://b2b.services.wasakredit.se/auth/connect/token'),
+        'base_url' => getEnvOrDefault('WASA_URI', 'https://b2b.services.wasakredit.se'),
+        'access_token_url' => getEnvOrDefault('WASA_AUTH_URI', 'https://b2b.services.wasakredit.se/auth/connect/token'),
         'test_base_url' => getEnvOrDefault('WASA_TEST_URI', 'http://st1-b2b.services.wasakredit.se'),
-        //'test_base_url' => getEnvOrDefault('WASA_TEST_URI', 'http://localhost:5004'),
         'test_access_token_url' => getEnvOrDefault('WASA_TEST_AUTH_URI', 'http://st1-b2b.services.wasakredit.se/auth/connect/token'),
-        // 'test_access_token_url' => getEnvOrDefault('WASA_TEST_AUTH_URI', 'http://wkcloud-front-lb-1268786071.eu-central-1.elb.amazonaws.com:83/'),
         'version' => 'php-2.5',
         'plugin' => null
-    ];  
+    ];
 
     return isset($wasa_configuration[$key]) ? $wasa_configuration[$key] : null;
 }
