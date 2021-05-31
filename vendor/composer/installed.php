f8abd9a64e8efe71682defb8882e41d0e8a2935c
Niklas Högefjord <niklas@krokedil.se>
2021-05-31 16:49:02 +0200

    Add vendor folder to version control so Deployment to Wordpress.org can work

---
+++ vendor/composer/installed.php
@@ -0,0 +1,34 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6078b6f2be85110d9e99c4ca8a5f743a5668b140',
+    'name' => 'wasa/woocommerce-plugin',
+  ),
+  'versions' => 
+  array (
+    'wasa/client-php-sdk' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '5e36a430694e7f1d3d73c72968ee5d960744b306',
+    ),
+    'wasa/woocommerce-plugin' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6078b6f2be85110d9e99c4ca8a5f743a5668b140',
+    ),
+  ),
+);
