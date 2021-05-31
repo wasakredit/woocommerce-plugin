5c02777677d190d5990b1e9770b14491202e96be
Niklas Högefjord <niklas@krokedil.se>
2021-05-31 19:32:45 +0200

    Remove sdk from Vendor

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -25,34 +25,24 @@ class InstalledVersions
 private static $installed = array (
   'root' => 
   array (
-    'pretty_version' => 'dev-master',
-    'version' => 'dev-master',
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
     'aliases' => 
     array (
     ),
-    'reference' => '6078b6f2be85110d9e99c4ca8a5f743a5668b140',
+    'reference' => '767a98ceba2f6d7e9ba0a785aca03c6da26f8a7f',
     'name' => 'wasa/woocommerce-plugin',
   ),
   'versions' => 
   array (
-    'wasa/client-php-sdk' => 
-    array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
-      'aliases' => 
-      array (
-        0 => '9999999-dev',
-      ),
-      'reference' => '5e36a430694e7f1d3d73c72968ee5d960744b306',
-    ),
     'wasa/woocommerce-plugin' => 
     array (
-      'pretty_version' => 'dev-master',
-      'version' => 'dev-master',
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
       'aliases' => 
       array (
       ),
-      'reference' => '6078b6f2be85110d9e99c4ca8a5f743a5668b140',
+      'reference' => '767a98ceba2f6d7e9ba0a785aca03c6da26f8a7f',
     ),
   ),
 );
