821eb068172cba33b89db1e0408b42185f878f6b
Niklas Högefjord <niklas@krokedil.se>
2023-12-19 15:50:52 +0100

    Solves issue with monthly cost not displayed correctly for variations in some cases

--- assets/js/monthly-cost-widget.js
+++ assets/js/monthly-cost-widget.js
@@ -37,6 +37,12 @@ if ( ! window.wasaKreditMonthlyCostWidget ) {
     if ( wasaKreditParams !== undefined ) {
         jQuery( function ( $ ) {
             const widget = {
+                updating: false,
+                init: function() {
+                    $( document ).on( "found_variation", widget.onVariationChange)
+                    $( "form.cart" ).on( "change", "input.qty", widget.onQuantityChange)
+                },
+
                 update_monthly_widget: function ( price ) {
                     const url = new URL( wasaKreditParams.wasa_kredit_update_monthly_widget_url, window.location )
                     $.ajax( {
@@ -51,40 +57,50 @@ if ( ! window.wasaKreditMonthlyCostWidget ) {
                         success: function ( res ) {
                             const container = $( ".wasa-kredit-product-widget-container" )
                             container.replaceWith( $.parseHTML( res.data ) )
+                            widget.updating = false;
                         },
                         complete: function ( res ) {
                             console.log( res )
                         },
                     } )
                 },
-            }
 
-            $( document ).on( "found_variation", function ( e, variation ) {
-                let price = Math.round( variation.display_price )
-                const quantity = parseInt( $( "form.cart input[name=quantity]" ).val() )
-                if ( ! isNaN( quantity ) ) {
-                    price *= quantity
-                }
-                widget.update_monthly_widget( price )
-            } )
-
-            $( "form.cart" ).on( "change", "input.qty", function () {
-                const quantity = parseInt( $( this ).val() )
-
-                let price = $( "form.cart .woocommerce-variation-price .amount" )
-                if ( 0 === price.length ) {
-                    price = $( ".summary .price .amount" )
-                }
-
-                const unit_price = parseFloat( price.text().replace( /[^\d.,]+/g, "" ) )
-
-                if ( quantity > 0 ) {
-                    const total_price = quantity * unit_price
-                    if ( ! isNaN( total_price ) ) {
-                        widget.update_monthly_widget( total_price )
+                onQuantityChange: function () {
+                    if( widget.updating ) {
+                        return;
+                    } 
+                    widget.updating = true;
+                    const {thousand_separator, decimal_separator} = wasaKreditParams;
+                    const quantity = parseInt( $( this ).val() )
+    
+                    let price = $( "form.cart .woocommerce-variation-price .amount" )
+                    if ( 0 === price.length ) {
+                        price = $( ".summary .price .amount" )
                     }
+                    const unit_price = parseFloat( price.text().replace( thousand_separator, "" ).replace(decimal_separator, '.') );
+                    if ( quantity > 0 ) {
+                        const total_price = quantity * unit_price
+                        if ( ! isNaN( total_price ) ) {
+                            widget.update_monthly_widget( total_price )
+                        }
+                    }
+                },
+
+                onVariationChange: function(e, variation) {
+                    if( widget.updating ) {
+                        return;
+                    } 
+                    widget.updating = true;
+
+                    let price = Math.round( variation.display_price )
+                    const quantity = parseInt( $( "form.cart input[name=quantity]" ).val() )
+                    if ( ! isNaN( quantity ) ) {
+                        price *= quantity
+                    }
+                    widget.update_monthly_widget( price )
                 }
-            } )
-        } )
+            }
+            widget.init();
+        })
     }
 }
